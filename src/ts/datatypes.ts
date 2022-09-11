4a12e314aa310eb7272bcf6d33b96066add607e9
Andrew Ensley <aensley@users.noreply.github.com>
2022-09-11 12:43:32 -0500

    Fix security (#4)

--- src/ts/datatypes.ts
+++ src/ts/datatypes.ts
@@ -1,16 +1,3 @@
-// Advent begins 4 Sundays before Christmas
-
-// Christmas ends on the Feast of the Baptism
-// Pope Paul VI set its date as the first Sunday after January 6 (as early as January 7 or as late as January 13) or, if in a particular country the Epiphany is celebrated on Sunday January 7 or Sunday January 8, on Monday January 8 or Monday January 9.
-
-// Lent begins on Ash Wednesday and ends on Holy Saturday
-
-// Easter lasts until Pentecost
-
-// Ordinary time is all time from Christmas to Lent and Pentecost to Advent
-
-// Weekday cycle does not apply to Advent, Christmas, Lent, or Easter
-
 export enum RosarySeries {
   Glorious = 'Glorious',
   Joyful = 'Joyful',
